1dc553fd5b5b5764f71fbdf7ef3d53d2208db18e
Julia Shishik <oiseua@gmail.com>
2017-02-27 10:11:04 +0200

    'devionity'

---
+++ OOP/polymorf.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Али
+ * Date: 25.02.2017
+ * Time: 11:04
+ */
+
+//'Полиморфизм';
+
+
+class BaseUser
+{
+    private $firstName;
+    private $lastName;
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    public  function setFirstName($value)
+
+    {
+        $this->firstName = $value;
+        return $this;
+    }
+    public  function getLastName()
+    {
+        return $this->lastName;
+    }
+    public  function setLastName($value)
+    {
+        $this->lastName = $value;
+        return $this;
+    }
+}
+
+class Admin extends BaseUser
+
+{
+
+    private  function  canDelete()
+    {
+        return true;
+    }
+}
+
+class Moderator  extends Admin
+{
+    protected $firstName;
+
+private  function canDelete()
+{
+    return false;
+}
+}
+
+
+
+class User extends BaseUser
+{
+
+}
+
+$admin = new Admin();
+$admin->setFirstName('Andrew');
+$admin->setLastName ('Popov');
+echo $admin->getFirstName();
+echo $admin->getLastName();
+
+$user = new User();
+$user->setFirstName('Andrew');
+$user->setLastName ('Popov');
+echo $user->getFirstName();
+echo $user->getLastName();
+
+$moderator = new Moderator();
+$moderator->setFirstName('Andrew');
+$moderator->setLastName ('Popov');
+echo $moderator->getFirstName();
+echo $moderator->canDelete();
---
+++ mysql/mysql_devionity/connect.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// procedure style:
+//$dbh = mysqli_connect('127.0.0.1', 'root', '', 'practice_db');
+
+// oo-style
+//$mysqli = new mysqli('127.0.0.1', 'root', '', 'practice_db');
+
+
+//devionity
+//$dbh = mysqli_connect('127.0.0.1', 'root', '', 'classicmodels');
+$mysqli = new mysqli('127.0.0.1', 'root', '', 'classicmodels');
+
+$db_host = '127.0.0.1';
+$db_user = 'root';
+$db_password = '';
+$db_name = 'classicmodels';
